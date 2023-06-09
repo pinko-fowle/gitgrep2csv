09af281f9ec16eabf2a741c090e5d69cfae0cd83
pinko-fowle <mfowle@socialtables.com>
2023-06-09 19:21:14 -0400

    docs: update index, regen README

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
+export * from "./config.js";
+export * from "./csv.js";
+export * from "./git.js";
+export * from "./group.js";
+export * from "./lines.js";
 export * from "./parse.js";
-export * from "./main.js";
+export * from "./types.js";
 
 // helper/utils
 import * as streams from "./streams.js";
